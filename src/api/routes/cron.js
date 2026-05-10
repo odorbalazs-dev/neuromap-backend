859c32a16659391e604b31f8d4602acea0fb37c8
Odor Balazs <odor.balazs@gmail.com>
2026-05-10 17:44:41 +0200

    add cron debug endpoint

--- src/api/routes/cron.js
+++ src/api/routes/cron.js
@@ -1,14 +1,13 @@
 import express from "express";
 
 import {
-  recoverAbandonedCheckouts
+  recoverAbandonedCheckouts,
+  debugCronSecret
 } from "../controllers/cron.controller.js";
 
 const router = express.Router();
 
-router.post(
-  "/recover-checkouts",
-  recoverAbandonedCheckouts
-);
+router.post("/recover-checkouts", recoverAbandonedCheckouts);
+router.get("/debug-secret", debugCronSecret);
 
 export default router;
